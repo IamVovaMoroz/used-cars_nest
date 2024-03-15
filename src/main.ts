57b92cc1974da5e46d51dc2f7abbf5e5456507fa
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-03-15 17:47:39 +0100

    ok

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+// import * as cookieSession from 'cookie-session';
+const cookieSession = require('cookie-session')
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      name: 'session',
+      keys: ['key1', 'key2'],
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      secure: false, // true in production for HTTPS
+      httpOnly: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
